30e9a5b48677822c2fcdfa503dba92c9f5d01fed
Lauricio Su <me@lauricio.com>
2015-11-11 16:11:32 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Wrapper for apn node package
---
+++ package.js
@@ -0,0 +1,33 @@
+Package.describe({
+  name: 'tapfuse:apn',
+  version: '1.6.2_1',
+  // Brief, one-line summary of the package.
+  summary: 'Wrapper for apn node package',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+var C = 'client';
+var S = 'server';
+var CS = [C, S];
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.0.1');
+
+  Npm.depends({
+    "apn": "1.6.2"
+  });
+
+  api.addFiles('lib/tapfuse-apn.js', S);
+  api.export('APN', S);
+
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('tapfuse:apn');
+  api.addFiles('tests/package-tests.js');
+});
