c5bfb96d4a839e77692f26f472871547a4b5f548
Lauricio Su <me@lauricio.com>
2015-11-11 16:21:39 +0700

    version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'tapfuse:apn',
-  version: '1.6.2_1',
+  version: '1.7.5_1',
   // Brief, one-line summary of the package.
   summary: 'Wrapper for apn node package',
   // URL to the Git repository containing the source code for this package.
@@ -14,12 +14,12 @@ var C = 'client';
 var S = 'server';
 var CS = [C, S];
 
+Npm.depends({
+  "apn": "1.7.5"
+});
+
 Package.onUse(function(api) {
   api.versionsFrom('1.2.0.1');
-
-  Npm.depends({
-    "apn": "1.6.2"
-  });
 
   api.addFiles('lib/tapfuse-apn.js', S);
   api.export('APN', S);
